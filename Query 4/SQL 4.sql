67bb54f2b981cecc32dd9f484a81328ff1ac3558
MabelChan2023 <mabelchan2023@gmail.com>
2023-08-03 12:33:24 -0400

    Change Query 4

--- Query 4/SQL 4.sql
+++ Query 4/SQL 4.sql
@@ -1,7 +1,6 @@
 /*
 Objective:
-Retrieve Comedy films with film length longer than 100 mins 
-and average rental payment higher than 5 in April in descending order
+Retrieve the average rental payment of Comedy films with film length longer than 90 mins in April in descending order
 */
 
 SELECT f.title,CAST(AVG(p.amount)AS DECIMAL(10,2))AS average_rental_payment
@@ -9,13 +8,11 @@ FROM category c
 
 JOIN film_category fc ON c.category_id = fc.category_id
 JOIN film f ON fc.film_id = f.film_id
-LEFT JOIN inventory i ON i.film_id = fc.film_id
-LEFT JOIN rental r ON r.inventory_id = i.inventory_id
-LEFT JOIN payment p ON p.rental_id = r.rental_id
+JOIN inventory i ON i.film_id = fc.film_id
+JOIN rental r ON r.inventory_id = i.inventory_id
+JOIN payment p ON p.rental_id = r.rental_id
 
-WHERE c.name='Comedy' AND f.length >100 AND EXTRACT(MONTH FROM p.payment_date) = 4
+WHERE c.name='Comedy' AND f.length >90 AND EXTRACT(MONTH FROM p.payment_date) = 4
 GROUP BY f.title, EXTRACT(MONTH FROM p.payment_date)
-HAVING AVG(p.amount) > 5
 ORDER BY average_rental_payment DESC
 ;
-
